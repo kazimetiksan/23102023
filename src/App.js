07d11c383cbf761e95e3795e5d348f54c247efa2
Kazım Etiksan <kazim@pikselmutfak.com>
2023-10-23 11:36:47 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Button from './Button';
+
+const App = () => {
+
+  const userInfo = {
+    firstName: "Kazım",
+    lastName: "Etiksan"
+  }
+
+  const someMethod = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          Merhaba {userInfo.firstName}
+        </div>
+        <Button title="Hello" /> {/* JSX - Prop */}
       </header>
     </div>
   );
---
+++ src/Button.js
@@ -0,0 +1,13 @@
+const Button = ({
+    title
+}) => {
+
+    // Javascript ES6
+    // Destructuring
+
+    return (
+        <button>{title}</button>
+    )
+}
+
+export default Button
