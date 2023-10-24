3ce6be0db1bf30a4aa8ce98989f8bd0ec739dce9
Kazım Etiksan <kazim@pikselmutfak.com>
2023-10-24 14:48:15 +0300

    redux detail page

--- src/Detail.js
+++ src/Detail.js
@@ -2,14 +2,26 @@ import {
     useParams
 } from 'react-router-dom'
 
+import { useSelector } from 'react-redux'
+
 const Detail = () => {
 
     const {_id} = useParams()
 
+    // const selectedUser = useSelector((state) => {
+
+    //     return state.user.find((item) => {
+
+    //         return item._id === _id
+    //     })
+    // })
+
+    const selectedUser = useSelector(state => state.user.find(item => item._id === _id))
+
     return (
         <div style={{
             margin: 50
-        }}>Detail {_id}</div>
+        }}>Merhaba {selectedUser.firstName}</div>
     )
 }
 
--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -3,10 +3,12 @@ import {
 } from '@reduxjs/toolkit'
 
 const initialState = [{
+    _id: "abc",
     firstName: "Kazım",
     lastName: "Etiksan",
     age: 33
   }, {
+    _id: "def",
     firstName: "Elif",
     lastName: "Yavuz",
     age: 43
